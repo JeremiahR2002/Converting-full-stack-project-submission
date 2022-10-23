cfcb32a41cdc8b33942f283b734381cf0e13e339
danielf9403 <103592213+danielf9403@users.noreply.github.com>
2022-10-23 10:42:58 -0400

    gg

--- Quiz_App_Backend/controller/router.js
+++ Quiz_App_Backend/controller/router.js
@@ -1,6 +1,15 @@
 const router = require('express').Router()
 
-
+router.get('/', (req, res) => {
+    db.Place.find()
+    .then((places) => {
+      res.render('places/index', { places })
+    })
+    .catch(err => {
+      console.log(err) 
+      res.render('error404')
+    })
+})
 
 
 module.exports = router
--- Quiz_App_Backend/server.js
+++ Quiz_App_Backend/server.js
@@ -3,6 +3,9 @@ const express = require('express')
 const methodOverride = require('method-override')
 const app = express()
 
+app.get('/', (req, res) => {
+    res.send('SUP G')
+})
 
 app.listen(process.env.PORT, () => {
     console.log("I am alive port " + process.env.PORT)
