87a50c993a934d44fd3a6cbd8f00c0f413499511
Garrison Hinshaw <garrison.hinshaw18@gmail.com>
2022-10-27 20:12:29 -0400

    connected backend to frontend

--- Quiz_App-Frontend/src/pages/Home.js
+++ Quiz_App-Frontend/src/pages/Home.js
@@ -2,24 +2,39 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
-export function Home() {
 
-  const getData = () => {
+  const Home = () => {
     console.log("We got clicked!");
-    window.location.assign("http://localhost:3000/Quiz");
+    const [name, setName]= useState('')
+    const [body, setBody] = useState('')
+    
+
+    const saveName = async () => {
+        const requestOptions = {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            content: name,
+          })
+        };
+      const posts = await fetch('/quizzes', requestOptions)
+      const cleanPosts = await posts.json()
   }
 
+console.log("name", name)
 return (
     <Form>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Display Name</Form.Label>
-        <Form.Control type="displayname" placeholder="Enter your display name!" />
+        <Form.Control onChange={(e)=> {
+          console.log('WE R TYPING on change happening!!')
+          setName(e.target.value)
+          }} type="displayname" placeholder="Enter your display name!" />
         <Form.Text className="text-muted">
-          Enter your Display Name and proceed!
+          Enter your Display Name and proceed! !!!!!!!!!!
         </Form.Text>
       </Form.Group>
-      <Button onClick={getData}  variant="primary">
-
+      <Button id="submitButton" onClick={saveName}  variant="primary">
         Submit
       </Button>
     </Form>
@@ -28,3 +43,4 @@ return (
 }
 
   
+export default Home
--- Quiz_App_Backend/server.js
+++ Quiz_App_Backend/server.js
@@ -5,29 +5,31 @@ const mongoose = require('mongoose');
 const app = express();
 const router = require('./controller/router')
 
+app.use(express.json())
+
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
     () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
   )
   
-app.use('/router', router)
+app.use('/quizzes', router)
 
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
+// app.get('/', (req, res) => {
+//     res.render('home')
+// })
 
-app.get('*', (req, res) => {
-    res.render('quizzes')
-})
+// app.get('*', (req, res) => {
+//     res.render('quizzes')
+// })
 
-app.get('*', (req, res) => {
-    res.render('result')
-})
+// app.get('*', (req, res) => {
+//     res.render('result')
+// })
 
-app.get('*', (req, res) => {
-    res.render('<h1>404 Page </h1>')
-})
+// app.get('*', (req, res) => {
+//     res.render('<h1>404 Page </h1>')
+// })
 
 app.listen(process.env.PORT, () => {
     console.log("I am alive port " + process.env.PORT)
