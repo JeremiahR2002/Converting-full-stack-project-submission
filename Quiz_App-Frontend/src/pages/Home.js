5fae67b403c8456f0e8d1173fe9a756dcf3d3b5b
Garrison Hinshaw <garrison.hinshaw18@gmail.com>
2022-11-01 17:51:50 -0400

    commited

--- Quiz_App-Frontend/src/pages/Home.js
+++ Quiz_App-Frontend/src/pages/Home.js
@@ -1,82 +1,85 @@
-import React, { useState, useEffect } from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import { useNavigate } from 'react-router-dom';
-import AllPosts from '../Components/allPosts';
+import React, { useState, useEffect } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { useNavigate } from "react-router-dom";
+import AllPosts from "../Components/allPosts";
 
+const Home = () => {
+  console.log("We got clicked!");
+  const [name, setName] = useState("");
+  const [body, setBody] = useState("");
+  const [comment, setComment] = useState("");
+  const [commentTitle, title] = useState("");
+  const [commentsData, setCommentsData] = useState([]);
 
-  const Home = () => {
-    console.log("We got clicked!");
-    const [name, setName]= useState('')
-    const [body, setBody] = useState('')
-    const [comment, setComment] = useState('')
-    const [commentTitle, title] = useState('')
-    const [commentsData, setCommentsData] = useState([])
-    
-    const navigate = useNavigate()
-    function navigateToQuiz(){
-      navigate("/Quiz")
-    };
-    const saveName = async () => {
-        const requestOptions = {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({
-            content: name,
-          })
-        };
-      const posts = await fetch('/quizzes', requestOptions)
-      //const cleanPosts = await posts.json()
-      document.getElementById('displayNameBox').value=""
-      navigateToQuiz();
-
+  const navigate = useNavigate();
+  function navigateToQuiz() {
+    navigate("/Quiz");
   }
-
-console.log("name", name)
-
-const grabComments = async () => {
-  console.log('Grab comments !!!!')
-  const posts = await fetch('/quizzes/comment')
-  const cleanPosts = await posts.json()
-  setCommentsData(cleanPosts)
-}
-
-const saveComment = async () => {
-  const requestOptions = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      title: commentTitle,
-      body: comment
-    })
+  const saveName = async () => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        content: name,
+      }),
+    };
+    const posts = await fetch("/quizzes", requestOptions);
+    //const cleanPosts = await posts.json()
+    document.getElementById("displayNameBox").value = "";
+    navigateToQuiz();
   };
-  const data = await fetch('/quizzes/comment', requestOptions)
-  const cleanData = await data.json()
-  console.log('We saved!', cleanData)
-  grabComments()
-  document.getElementById('commentBox').value=""
-  document.getElementById('commentNameBox').value=""
-}
 
-useEffect(()=> {
-  grabComments()
-},[])
+  console.log("name", name);
 
-//console.log('comments', comment)
-console.log('commentsData', commentsData)
-return (
+  const grabComments = async () => {
+    console.log("Grab comments !!!!");
+    const posts = await fetch("/quizzes/comment");
+    const cleanPosts = await posts.json();
+    setCommentsData(cleanPosts);
+  };
+
+  const saveComment = async () => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        title: commentTitle,
+        body: comment,
+      }),
+    };
+    const data = await fetch("/quizzes/comment", requestOptions);
+    const cleanData = await data.json();
+    console.log("We saved!", cleanData);
+    grabComments();
+    document.getElementById("commentBox").value = "";
+    document.getElementById("commentNameBox").value = "";
+  };
+
+  useEffect(() => {
+    grabComments();
+  }, []);
+
+  //console.log('comments', comment)
+  console.log("commentsData", commentsData);
+  return (
     <Form>
       <Form.Group className="mb-3" controlId="formBasicEmail">
-      <Form.Text className="text-muted">
+        <Form.Text className="text-muted">
           Enter your Display Name and proceed!
         </Form.Text>
         <br></br>
-        <Form.Control id="displayNameBox" onChange={(e)=> {
-          console.log('WE R TYPING on change happening!!')
-          setName(e.target.value)
-          }} type="displayname" placeholder="Display Name" />
+        <Form.Control
+          id="displayNameBox"
+          onChange={(e) => {
+            console.log("WE R TYPING on change happening!!");
+            setName(e.target.value);
+          }}
+          type="displayname"
+          placeholder="Display Name"
+        />
       </Form.Group>
-      <Button id="submitButton" onClick={saveName}  variant="primary">
+      <Button id="submitButton" onClick={saveName} variant="primary">
         Submit
       </Button>
       <br></br>
@@ -84,31 +87,39 @@ return (
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Comment Section</Form.Label>
         <br></br>
-        <Form.Control id="commentNameBox" onChange={(e)=> {
-          console.log('WE R TYPING on change happening!!')
-          title(e.target.value)
-          }} type="commentsection" placeholder="Display Name" />
-        <Form.Text className="text-muted">
-        </Form.Text>
+        <Form.Control
+          id="commentNameBox"
+          onChange={(e) => {
+            console.log("WE R TYPING on change happening!!");
+            title(e.target.value);
+          }}
+          type="commentsection"
+          placeholder="Display Name"
+        />
+        <Form.Text className="text-muted"></Form.Text>
         <br></br>
         <br></br>
-        <Form.Control id="commentBox" onChange={(e)=> {
-          console.log('WE R TYPING on change happening!!')
-          setComment(e.target.value)
-          }} type="commentsection" placeholder="Enter your comment!" />
+        <Form.Control
+          id="commentBox"
+          onChange={(e) => {
+            console.log("WE R TYPING on change happening!!");
+            setComment(e.target.value);
+          }}
+          type="commentsection"
+          placeholder="Enter your comment!"
+        />
       </Form.Group>
       <Button id="submitButton" onClick={saveComment} variant="primary">
         Submit a comment!
       </Button>
       <Form.Text>
-      <h3>Comments</h3>
-      <AllPosts comments={commentsData}/>
-    </Form.Text>
+        <h3>Comments</h3>
+        <AllPosts comments={commentsData} />
+      </Form.Text>
     </Form>
 
-
-// pushing test
+    // pushing test
   );
-        }
-  
-export default Home
+};
+
+export default Home;
